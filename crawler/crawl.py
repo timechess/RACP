99d2feb8b477444f2989bab8d00f30158be5b1ca
timechess <2535241661@qq.com>
2023-11-06 21:02:33 +0800

    Init crawler

---
+++ crawler/crawl.py
@@ -0,0 +1,16 @@
+# This file implements PaperCrawler, which crawl pdfs from the Internet, parse them and save data
+# into specified directory. 
+
+
+class PaperCrawler:
+    def __init__(self) -> None:
+        pass
+
+    def crawl(self):
+        pass
+
+    def parse(self):
+        pass
+
+    def save(self):
+        pass
---
+++ crawler/middleware.py
@@ -0,0 +1,8 @@
+# This file implements PaperMiddleware, which process response from the remote server and
+# process the pdfs.
+
+
+
+class PaperMiddleware:
+    def __init__(self) -> None:
+        pass
