5df60bbdfd1c3940b8a1da61392102caf335c9c7
timechess <2535241661@qq.com>
2023-11-20 21:16:35 +0800

    Update `crawl` module
    Now the download process only takes paper id instead of actual link. It can crawl several fields together and all the papers in a month can be detected instead of a fixed number. It's easier to only crawl abstract.

--- example/crawl.py
+++ example/crawl.py
@@ -11,7 +11,8 @@ with open("resources\stop_words.txt", "r", encoding="utf-8") as f:
 
 def parser():
     parser = argparse.ArgumentParser("Script for crawling from arXiV")
-    parser.add_argument("--field", type=str, default="cs.IR", help="Field to crawl")
+    parser.add_argument("--mode", type=str, choices=["pdf", "abs", "all"], help="Which to download")
+    parser.add_argument("--fields", default=["cs.IR"],nargs="+", type=str, help="Fields to crawl")
     parser.add_argument("--get-link", default=False, action="store_true", help="Get pdf links and save")
     parser.add_argument("--threads", type=int, default=4, help="Threads used to download pdfs")
     parser.add_argument("--check-download", default=False, action="store_true", help="Whether only check download")
@@ -33,39 +34,44 @@ makedir(os.path.join(arg.save_path,"abs"), logger)
 
 
 def pdf_download_worker(split, id):
-    crawl.download_pdf(split[id],arg.save_path,logger,False, stopwords)
+    crawl.download(split[id],"pdf",arg.save_path,logger)
 
 def abs_download_worker(split, id):
-    crawl.download_abs(split[id],arg.save_path,logger)
+    crawl.download(split[id],"abs",arg.save_path,logger)
 
 
 if __name__ == "__main__":
     if arg.get_link:   
-        pdflinks = crawl.get_links(3, arg.field, arg.save_path, logger)
+        pdflinks = crawl.get_links(3, arg.fields, arg.save_path, logger)
     else:
         try:
             with open(os.path.join(arg.save_path, "pdflinks.json"),"r") as f:
                 pdflinks = json.load(f)
         except:
-            pdflinks = crawl.get_links(3, arg.field, arg.save_path, logger)
+            pdflinks = crawl.get_links(3, arg.fields, arg.save_path, logger)
+
+    pdfids = list(map(lambda link: link.split("/")[-1], pdflinks))
+    absids = pdfids
     if arg.check_download:
-        pdflinks, abslinks = crawl.check_download(pdflinks, arg.save_path, logger)
-    pdfnum = len(pdflinks)//arg.threads
-    absnum = len(abslinks)//arg.threads
-    pdflink_split = [pdflinks[pdfnum*i:pdfnum*(i+1)] for i in range(arg.threads)]
-    abslink_split = [abslinks[absnum*i:absnum*(i+1)] for i in range(arg.threads)]
+        pdfids, absids = crawl.check_download(pdfids, arg.save_path, logger)
+    pdfnum = len(pdfids)//arg.threads
+    absnum = len(absids)//arg.threads
+    pdfid_split = [pdfids[pdfnum*i:pdfnum*(i+1)] for i in range(arg.threads)]
+    absid_split = [absids[absnum*i:absnum*(i+1)] for i in range(arg.threads)]
     if len(pdflinks)-pdfnum*arg.threads != 0:
-        pdflink_split[-1] += pdflinks[-(len(pdflinks)-pdfnum*arg.threads):]
-    if len(abslinks)-absnum*arg.threads != 0:
-        abslink_split[-1] += abslinks[-(len(abslinks)-absnum*arg.threads):]
+        pdfid_split[-1] += pdfids[-(len(pdfids)-pdfnum*arg.threads):]
+    if len(absids)-absnum*arg.threads != 0:
+        absid_split[-1] += absids[-(len(absids)-absnum*arg.threads):]
     threads = []
-    for i in range(arg.threads):
-        t = Process(target=abs_download_worker,args=(abslink_split, i))
-        t.start()
-        threads.append(t)
-    for i in range(arg.threads):
-        t = Process(target=pdf_download_worker,args=(pdflink_split, i))
-        t.start()
-        threads.append(t)
+    if arg.mode == "abs" or arg.mode == "all":
+        for i in range(arg.threads):
+            t = Process(target=abs_download_worker,args=(absid_split, i))
+            t.start()
+            threads.append(t)
+    elif arg.mode == "pdf" or arg.mode == "all":
+        for i in range(arg.threads):
+            t = Process(target=pdf_download_worker,args=(pdfid_split, i))
+            t.start()
+            threads.append(t)
     for t in threads:
         t.join()
--- src/racp/crawl.py
+++ src/racp/crawl.py
@@ -2,41 +2,88 @@ import requests
 from bs4 import BeautifulSoup
 from tqdm import tqdm
 import os
+import json
 import time
-from racp.utils import makedir, save_json, parse_pdf
+from racp.utils import save_json
+from loguru import logger
+
+logger.add(
+    "log.log",
+    enqueue=True,
+    level="ERROR"
+)
 
 def get_links(
         years : int, 
-        field : str, 
-        save_path : str, 
-        logger, 
+        fields : list, 
+        save_path : str,
+        logger=logger, 
         headers=None, 
-        timeout=10
-):
+        timeout=30
+    ):
     '''Get pdf links from specified field and years
     
     Given years and field, it crawls from arXiV and get all pdf links that satisfies requirements.
-    For example, get_links(3, cs.IR, ..) returns all cs.IR papers in 2021-2023 and saves data into
-    the given path. The json file is named pdflinks.json. Internet errors will be logged and pass.
+    For example, get_links(3, [cs.IR], ..) returns all cs.IR papers in 2021-2023 and saves data into
+    the given path. The json file is named "pdflinks.json". Internet errors will be logged and pass.
+    Note that we first need to get all urls to crawl since arXiV defaultly set one page contains 25
+    paper. The cache will be saved as "all_queries.json". If you want to crawl again, please delete
+    the cache.
 
     Args:
         years: The num of years you want to crawl from 2023.
-        field: The arXiV field of papers you want, like cs.IR, cs.CV.
+        fields: A List of arXiV fields of papers you want, like cs.IR, cs.CV.
         save_path: The path to save crawled data.
         logger: loguru logger.
         headers: Default to None.
-        timeout: Default to 10.
+        timeout: Default to 30.
 
     Returns:
         links: A List that contains all the pdf links needed.
     '''
 
-    time = ["{}{:02}".format(23-i,j) for i in range(years) for j in range(1,13)]
+    times = ["{}{:02}".format(23-i,j) for i in range(years) for j in range(1,13)]
     base_url = "https://arxiv.org/list"
-    queries = list(map(lambda query: query + "?show=500",["/".join([base_url,field,time[i]]) for i in range(len(time))]))
-
+    failed_cases = []
+    first_queries = []
+    all_queries = []
+    if os.path.exists(os.path.join(save_path, "all_queries.json")):
+        with open(os.path.join(save_path, "all_queries.json")) as f:
+            all_queries = json.load(f)
+    else:
+        for field in fields:
+            queries = ["/".join([base_url,field,times[i]]) for i in range(len(times))]
+            first_queries += queries
+        logger.debug("Start to construct all urls to crawl")
+        for url in tqdm(first_queries):
+            try:
+                res = requests.get(url, headers=headers, timeout=timeout)
+                res.raise_for_status()
+                bs = BeautifulSoup(res.text, features="xml")
+                paper_num = int(bs.find_all("small")[0].text.split(" ")[3])
+                queries = [url + f"?skip={100*i}&show=100" for i in range(paper_num//100+1)]
+                all_queries += queries
+                time.sleep(5)
+            except:
+                logger.error(f"Fail to get {url}")
+                failed_cases.append(url)
+                pass
+        logger.debug("Trying again to get failed cases")
+        for url in tqdm(failed_cases):
+            try:
+                res = requests.get(url, headers=headers, timeout=timeout)
+                res.raise_for_status()
+                bs = BeautifulSoup(res.text, features="xml")
+                paper_num = int(bs.find_all("small")[0].text.split(" ")[3])
+                queries = [url + f"?skip={100*i}&show=100" for i in range(paper_num//100+1)]
+                all_queries += queries
+                time.sleep(5)
+            except:
+                logger.error(f"Fail to get {url}")
+                pass
+        save_json(all_queries, os.path.join(save_path, "all_queries.json"),logger, "all_queries.json")
     links = []
-    for url in tqdm(queries):
+    for url in tqdm(all_queries):
         try:
             res = requests.get(url, headers=headers, timeout=timeout)
             res.raise_for_status()
@@ -47,7 +94,7 @@ def get_links(
         except:
             logger.error(f"Fail to get {url}")
             pass
-    
+    links = list(set(links))
     save_json(links, os.path.join(save_path, "pdflinks.json"), logger, "pdflinks.json")
     logger.info(f"Get {len(links)} pdf to crawl")
     return links
@@ -56,27 +103,18 @@ def get_links(
 def download_pdf(
         pdflinks : list, 
         save_path : str, 
-        logger, 
-        parse=False,
-        stopwords=None,
-        sleep=1,
-        headers=None
+        logger=logger, 
     ):
     '''Download pdf and abstract from the given list of links
     
     Given the pdf links to crawl, it download pdfs from the Internet and save them to given path.
-    For example, the pdf will be saved as pdf/{id}.pdf.You can choose to parse the pdf while 
-    downloading by pass parse=True. Now abstract won't be downloaded in this function. Check
-    `download_abs`.
+    For example, the pdf will be saved as pdf/{id}.pdf. Now abstract won't be downloaded in this 
+    function. Check `download_abs`.
 
     Args:
         pdflinks: A List of links to download.
         save_path: The path to save data.
         logger: loguru logger.
-        parse: Whether parse the pdf upon finishing download, default to False.
-        stopwords: List of stopwords used by tokenization, default to None.
-        sleep: Sleep time after finishing downloading one pdf, default to 1.
-        headers: Default to None.
     '''
 
     dir_path = os.path.join(save_path, "pdf")
@@ -84,7 +122,7 @@ def download_pdf(
     for link in pdflinks:
         pdf_id = link.split("/")[-1]
         try:
-            res = requests.get(link, headers=headers)
+            res = requests.get(link)
             res.raise_for_status()
             with open(os.path.join(dir_path, pdf_id+".pdf"), "wb") as f:
                 f.write(res.content)
@@ -95,20 +133,15 @@ def download_pdf(
             logger.error(f"Process {os.getpid()} : Fail to download {pdf_id}.pdf")
             continue
         #download_abs(pdf_id, save_path, logger)
-        if parse:
-            if stopwords == None:
-                raise ValueError("You need to pass in valid stopwords to parse pdf")
-            parse_pdf(os.path.join(dir_path,pdf_id+".pdf"), save_path, stopwords, logger)
-        time.sleep(sleep)
 
 def download_abs(
         abslinks : list,
         save_path : str,
-        logger
-):
+        logger=logger
+    ):
     '''Get the abstract of given arXiV paper abstrack links.
     
-    This function gets abstract of the given papers' links.
+    This function gets abstract of the given papers' links. It will save to `save_path`/abs/{pdf_id}.txt.
 
     Args:
         abslinks: A List of links to download.
@@ -122,9 +155,10 @@ def download_abs(
             res = requests.get(link)
             res.raise_for_status()
             abs_bs = BeautifulSoup(res.text, "xml")
-            abstract = abs_bs.find_all("blockquote")[0].text
+            abstract = abs_bs.find_all("blockquote")[0].text.replace("Abstract:", "")
             extra_content_index = abstract.find("\n\n\n")
             abstract = abstract[:extra_content_index]
+           
             with open(os.path.join(save_path, "abs", pdfid+".txt"), "w", encoding="utf-8") as f:
                 f.write(abstract)
             count += 1
@@ -132,39 +166,65 @@ def download_abs(
         except:
             logger.error(f"Process {os.getpid()} : Fail to get {pdfid}'s abstract")
 
-
-def check_download(
-        pdflinks : list,
+def download(
+        pdfids : list,
+        mode : str,
         save_path : str,
-        logger
-):
-    '''Check whether all pdf have been downloaded and download fail cases.
+        logger=logger
+    ):
+    '''A joint api for pdf and abs
     
-    Given a List of pdf links, it will check the `pdf` directory under `save_path`. It returns the
-    pdf links failed to download.
+    This function uses `download_pdf` and `download_abs`. It only needs papers' arXiV ids. You can 
+    specify which to download by passing the `mode` argument.
 
     Args:
-        pdflinks: List of pdf links.
+        pdfid: A List of arXiV ids of the papers you want.
+        mode: "pdf" or "abs" or "all".
+        save_path: The path to save data.
+        logger: loguru logger.
+    '''
+    pdflinks = [f"https://arxiv.org/pdf/{pdfid}" for pdfid in pdfids]
+    abslinks = [f"https://arxiv.org/abs/{pdfid}" for pdfid in pdfids]
+    if mode == "pdf":
+        download_pdf(pdflinks, save_path, logger)
+    elif mode == "abs":
+        download_abs(abslinks, save_path, logger)
+    elif mode == "all":
+        download_pdf(pdflinks, save_path, logger)
+        download_abs(abslinks, save_path, logger)
+    else:
+        raise ValueError("Unknown mode. Please select in ['pdf', 'abs', 'all']")
+    
+
+def check_download(
+        pdfids : list,
+        save_path : str,
+        logger=logger
+    ):
+    '''Check whether all pdf have been downloaded and download fail cases.
+    
+    Given a List of pdf ids, it will check the `pdf` directory under `save_path`. It returns the
+    pdf or abstract ids failed to download.
+
+    Args:
+        pdfids: List of pdf links.
         save_path: The path to save data, which should contain a pdf directory.
         logger: loguru logger.
 
     Returns:
-        pdf_failed_links: The links of failed pdfs.
-        abs_failed_links: The links of failed abstracts.
+        pdf_failed_ids: The ids of failed pdfs.
+        abs_failed_ids: The ids of failed abstracts.
     '''
     existing_abs = os.listdir(os.path.join(save_path, "abs"))
     existing_pdfs = os.listdir(os.path.join(save_path, "pdf"))
-    pdf_failed_links = []
-    abs_failed_links = []
-    for link in pdflinks:
-        pdfid = os.path.split(link)[-1]
+    pdf_failed_ids = []
+    abs_failed_ids = []
+    for pdfid in pdfids:
         if pdfid + ".pdf" not in existing_pdfs:
-            pdf_failed_links.append(pdfid)
+            pdf_failed_ids.append(pdfid)
         if pdfid + ".txt" not in existing_abs:
-            abs_failed_links.append(pdfid)
-    pdf_failed_links = list(map(lambda id: "https://arxiv.org/pdf/"+id, pdf_failed_links))
-    abs_failed_links = list(map(lambda id: "https://arxiv.org/abs/"+id, abs_failed_links))
-    logger.info(f"{len(pdf_failed_links)} pdfs are failed")
-    logger.info(f"{len(abs_failed_links)} abs are failed")
-    return pdf_failed_links, abs_failed_links
+            abs_failed_ids.append(pdfid)
+    logger.info(f"{len(pdf_failed_ids)} pdfs are failed")
+    logger.info(f"{len(abs_failed_ids)} abs are failed")
+    return pdf_failed_ids, abs_failed_ids
 
