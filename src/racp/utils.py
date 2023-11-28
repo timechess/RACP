b0f83b419604316df729384861aa7ea10e707d90
CIntellifusion <96377955+CIntellifusion@users.noreply.github.com>
2023-11-28 19:27:26 +0800

    ccbc function

--- src/racp/utils.py
+++ src/racp/utils.py
@@ -43,3 +43,34 @@ def save_json(
     logger.debug(f"Process {os.getpid()} : Saving {name} to {path}")
     with open(path, "w", encoding= "utf-8") as f:
         json.dump(obj, f, indent=4)
+        
+    
+def CCBC(paperA,paperB):
+    """Calculate citation similarity index. 
+    
+    CCBC stands for co-citation and bib coupling index 
+    plus direct citation as well 
+    
+    Args:
+        paperA: PaperItem 
+        paperB: PaperItem
+    
+    Returns:
+        index: [0,1.5]
+    """
+    score = 0 
+    # 1. direct citation relationship 
+    if paperA.paperId in paperB.citations or \
+        paperB.paperId in paperA.citations:
+            score += 0.5 
+    # 2. shared citation ratio 
+    cocite = paperA.citations and  paperB.citations
+    alcite = paperA.citations or   paperB.citations
+    score += len(cocite) / len(alcite)
+    # 3. shared reference ratio 
+    coref = paperA.references and  paperB.references
+    alref = paperA.references or   paperB.references
+    score += len(coref) / len(alref)
+    
+    return score  
+
