1acc991b1c0b91d2cc9e55f885ef14adc9af31b6
CIntellifusion <96377955+CIntellifusion@users.noreply.github.com>
2023-12-04 00:13:02 +0800

    table demo

--- webui/app.py
+++ webui/app.py
@@ -12,8 +12,8 @@ import argparse
 import yaml
 from naive_retriver import Retriver
 from racp.data import PaperItem ,RawSet
-from racp import util 
-config = util.load_config("./retriver_config.yaml")
+from racp import utils 
+config = utils.load_config("./retriver_config.yaml")
 retriver = Retriver(config)
 database = RawSet(config.dbpath)
 
@@ -27,13 +27,18 @@ def process_text_and_file(input_text, uploaded_file):
     else:
         return ""
 
-def process_arxiv_id(arxiv_id,k=100):
+def process_arxiv_id(arxiv_id,k=1000):
     # 根据arxiv id 爬 pdf -> 文档 
     try:
         paper = PaperItem(arxiv_id=arxiv_id)
         topkitems = database.topk(paper,k=k)
+        topkpaper = database()
+        topkpaper.load_from_papers(topkitems)
         # build retriver 
-        local_retriver = Retriver(config)
+        local_retriver = Retriver(config,topkpaper)
+        result = local_retriver.retrival(paper.abstract,k=10)
+        
+        
     except ConnectionError as e:
         result = e 
     
@@ -65,9 +70,18 @@ def index():
         result = retriver.retrival(processed_text)
 
         # 将arXiv结果存储在session中
+        # TODO : result and table data demo 
         session['retrieval_results'] = result
+        # 模拟获取 Papername、arXiv ID、quality 和 relevance 数据的列表
+        table_data = [
+            {'Papername': 'Paper1', 'arxiv_id': '1234.5678', 'quality': 0.85, 'relevance': 0.92},
+            {'Papername': 'Paper2', 'arxiv_id': '5678.1234', 'quality': 0.75, 'relevance': 0.88},
+        ]
 
-        return render_template('index.html', input_text=input_text, processed_text=result, arxiv_result=arxiv_result)
+        # 将结果存储在 session 中，以便在下一次请求时使用
+        session['processed_text'] = processed_text
+        session['table_data'] = table_data
+        return render_template('index.html', input_text=input_text, processed_text=processed_text, table_data=table_data)
 
     # 如果是GET请求，清空之前的结果
     session.pop('processed_text', None)
